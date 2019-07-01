66b7a8abbd8d7daab6cb57f4980348c7d7f2f560
Zhan Zhang <klosezhag@yahoo.com.au>
2019-07-01 23:38:59 +1000

    clean up project folder. Move old coding practise into old directory

--- src/client.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var service = require('./service'); //Load greet.js module and assign it to varible greet
-var name = 'Zhan';
-service.sayGreet(name); //Now call the method exported in greet.js mudule
-service.sayGoodbye(name);
